70fb7762b090311bb16ef62b74fcdf7e1fa3482f
ChristianDannesboe <dannesboe@live.dk>
2016-10-24 13:18:57 +0200

    WifiShield

---
+++ arduino/Connect to wifi/connect_and_print/connect_and_print.ino
@@ -0,0 +1,40 @@
+#include <WiFi.h>
+
+char ssid[] = "Mark Klochs iPhone";     //  your network SSID (name) 
+char pass[] = "markersej";    // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+IPAddress ip;
+WiFiServer server(80);
+WiFiClient client;
+int message;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+
+  // attempt to connect using WPA2 encryption:
+  Serial.println("Attempting to connect to WPA network...");
+  status = WiFi.begin(ssid, pass);
+
+  // if you're not connected, stop here:
+  if ( status != WL_CONNECTED) { 
+    Serial.println("Couldn't get a wifi connection");
+    while(true);
+  } 
+  // if you are connected, print out info about the connection:
+  else {
+    server.begin();
+    Serial.println("Connected to network");
+  }
+  ip = WiFi.localIP();
+  Serial.println(ip);
+}
+
+void loop() {
+  delay(1000);
+  client = server.available();
+  if(client != false){
+    message = client.read();
+    Serial.println(message);
+  }
+}
