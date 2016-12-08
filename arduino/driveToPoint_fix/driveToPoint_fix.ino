622b8f69fb6a3d4d8c9f4584c9b77e415264a489
bovle <frederik.bl@live.dk>
2016-12-08 09:23:07 +0100

    driveToPoint med fix_t

--- arduino/driveToPoint_fix/driveToPoint_fix.ino
+++ arduino/driveToPoint_fix/driveToPoint_fix.ino
@@ -1,3 +1,9 @@
+extern "C"{
+  #include <avrfix.h>
+}
+#include <AFMotor.h>
+#include <Math.h>
+
 // Adafruit Motor shield library
 // copyright Adafruit Industries LLC, 2009
 // this code is public domain, enjoy!
@@ -8,11 +14,9 @@
 
 volatile int leftTotal = 0;
 volatile int rightTotal = 0;
-bool motorLeftRun = false;
-bool motorRightRun = false;
-double heading = 0.0, posX = 0.0, posY = 0.0, margin = 10.0;
-unsigned int tid;
-int leftTemp, rightTemp, goalX = 230, goalY = 330, loopcount = 0, signalCount = 0;
+bool atGoal = false;
+fix_t heading, posX, posY, margin, goalX, goalY; 
+int leftTemp, rightTemp;
 
 #include <AFMotor.h>
 #include <Math.h>
@@ -27,10 +31,17 @@ void setup() {
   attachInterrupt(3, incrementLeft, CHANGE);
   pinMode(RIGHTENCODERPIN, INPUT);
   attachInterrupt(2, incrementRight, CHANGE);
+
+  heading = ftok(0.0);
+  posX = ftok(0.0);
+  posY = ftok(0.0);
+  margin = ftok(10.0);
+  goalX = ftok(230.0);
+  goalY = ftok(330.0);
+  
   // turn on motor
   motorLeft.setSpeed(255);
   motorRight.setSpeed(255);
-
 
   motorLeft.run(RELEASE);
   motorLeftRun = false;
@@ -56,40 +67,43 @@ void loop() {
 void updatePosAndHead(){
   int currentLeft = leftTotal;
   int currentRight = rightTotal;
-  double deltaLeft = (currentLeft - leftTemp) * DISTPRDEGREE;
-  double deltaRight = (currentRight - rightTemp) * DISTPRDEGREE;
+  fix_t distPrDeg = ftok(DISTPRDEGREE);
+  fix_t dltL = itok(currentLeft - leftTemp);
+  fix_t dltR = itok(currentRight - rightTemp);
+  fix_t deltaLeft = mulk(dltL, distPrDeg);
+  fix_t deltaRight = mulk(dltR, distPrDeg);
   leftTemp = currentLeft;
   rightTemp = currentRight;
-  double dist = (deltaLeft + deltaRight) / 2.0;
-  posX += (dist * sin(heading));
-  posY += (dist * cos(heading));
-  heading += (atan((deltaRight - deltaLeft) / WHEELDIST));
+  fix_t deltaSum = deltaLeft + deltaRight;
+  fix_t dist = divk(deltaSum,ftok(2.0));
+  fix_t sinHeading = sink(heading);
+  posX += mulk(dist, sinHeading);
+  fix_t cosHeading = cosk(heading);
+  posY += mulk(dist, cosHeading);
+  fix_t rel = divk((deltaRight - deltaLeft),ftok(WHEELDIST));
+  heading += atank(rel);
 }
 
 void driveTowardsGoal(){
-  double deltaX = goalX - posX;
-  double deltaY = goalY - posY;
-  double goalHeading = atan(deltaX/deltaY);
-  
-  double deltaHeading = goalHeading - heading;
-  if(deltaHeading < -0.1){
-    motorLeft.run(FORWARD);
-    motorRight.run(RELEASE);
-  }else if(deltaHeading > 0.1){
-    motorLeft.run(RELEASE);
-    motorRight.run(FORWARD);
-  }else{
-    motorLeft.run(FORWARD);
-    motorRight.run(FORWARD);
-  }
-}
-
-void stopIfAtGoal(){
   if(posX <= goalX + margin && posX >= goalX - margin && posY <= goalY + margin && posY >= goalY - margin){
     motorLeft.run(RELEASE);
     motorRight.run(RELEASE);
-    delay(50);
-    exit(0);
+  }else{
+    fix_t deltaX = goalX - posX;
+    fix_t deltaY = goalY - posY;
+    fix_t rel = divk(deltaX,deltaY);
+    fix_t goalHeading = atank(rel);
+    fix_t deltaHeading = goalHeading - heading;
+    if(deltaHeading < ftok(-0.1)){
+      motorLeft.run(FORWARD);
+      motorRight.run(RELEASE);
+    }else if(deltaHeading > ftok(0.1)){
+      motorLeft.run(RELEASE);
+      motorRight.run(FORWARD);
+    }else{
+      motorLeft.run(FORWARD);
+      motorRight.run(FORWARD);
+    }
   }
 }
 
